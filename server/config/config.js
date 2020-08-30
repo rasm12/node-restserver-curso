8e379e1b490b45ef3771c5763804b5ec63ed7d39
rsanchezmieres <rsanchezmieres@gmail.com>
2020-08-30 13:07:46 -0400

    variable env

--- server/config/config.js
+++ server/config/config.js
@@ -3,12 +3,13 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || "DEV";
 let urlDB;
 
-process.env.NODE_ENV = 'production';
 
 if (process.env.NODE_ENV === 'DEV') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://user_test:1234567890@cluster0.j5wpp.mongodb.net/';
+    urlDB = process.env.MONGO_URI;
 }
+
+console.log(process.env.NODE_ENV)
 
 process.env.urlDB = urlDB;
